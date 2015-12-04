367a7a4df6b665eb8e807906bbb1af068ee2007e
cforster <charlie@forsters.com>
2015-12-04 01:25:55 -0500

    added period highlighting and rounded corners

--- client/templates/cal.js
+++ client/templates/cal.js
@@ -3,7 +3,7 @@
  */
 Template.day.helpers({
     dayevents: function (dayparam) {
-        return Schedules.find({day: dayparam}).map(function(u) {
+        return Schedules.find({day: dayparam},{period:1}).map(function(u) {
             var starttime = dayschedules[u.day][u.period].start;
             var endtime = dayschedules[u.day][u.period].end;
             var starthour = 800;
@@ -14,3 +14,24 @@ Template.day.helpers({
         });
     }
 });
+
+Template.day.events({
+    'mouseenter.event': function (event) {
+        $(event.currentTarget.className.replace("event ",".")).css('background-color', 'red');
+    },
+    'mouseleave.event': function (event) {
+        $(event.currentTarget.className.replace("event ",".")).css('background-color', '');
+    }
+
+});
+
+
+//$(function() {
+//    $(".period-I").hover(function() {
+//        $(this.className.replace("event ",".")).css('background-color', 'red');
+//    }, function() {
+//        // on mouseout, reset the background colour
+//        $(this.className.replace("event ",".")).css('background-color', '');
+//    });
+//});
+//[#document]
